156683fd99d3b2d056094c57670e688eda3216bd
Vincent Cantin <vincent@404.taipei>
2025-02-09 13:06:29 +0200

    fixes the e2e tests run from Github

--- test/playwright/playwright.config.js
+++ test/playwright/playwright.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:8080",
+    baseURL: "http://127.0.0.1:8081",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -74,8 +74,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     timeout: 60 * 60 * 1000, // Waiting for shadow-cljs to download its dependencies.
-    command: "cd ../app && npm install && npm run release && npm run server",
-    url: "http://127.0.0.1:8080",
+    command: "cd ../app/signaa-gent-conf && npm install && npm run release && npm run server",
+    url: "http://127.0.0.1:8081",
     reuseExistingServer: !process.env.CI,
   },
 });
